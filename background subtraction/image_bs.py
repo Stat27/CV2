2c3f149fe73f9767c1a0864c909c8fb907a2bbf8
StevenX27 <fx4@ualberta.ca>
2023-02-13 12:10:23 -0700

    contrast

--- background subtraction/image_bs.py
+++ background subtraction/image_bs.py
@@ -1,27 +1,39 @@
 import numpy as np 
-import cv2 as cv 
-from matplotlib import pyplot as plt 
+import cv2
+from tkinter.filedialog import askdirectory
+import os
 
-img = cv.imread('./data/tank.jpg')
-fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
+folder = askdirectory()
+All_images = []
+# fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+# fgbg = cv2.bgsegm.BackgroundSubtractorGMG()
+# fgbg = cv2.createBackgroundSubtractorMOG2(detectShadows=True)
+fgbg = cv2.createBackgroundSubtractorKNN(detectShadows=False)
+for path, subdirs, files in os.walk(folder,topdown=True):
+    # iterates over every file in the selected directory
+    for filename in files:
+        src = os.path.join(path,filename)
+        img = cv2.imread(src)
+        All_images.append(img)
+        
 
-#fgbg = cv.bgsegm.BackgroundSubtractorGMG()
-#fgbg = cv.createBackgroundSubtractorMOG2(detectShadows=True)
-#fgbg = cv.createBackgroundSubtractorKNN(detectShadows=True)
 
-fgmask = fgbg.apply(img)
-#fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
+cv2.namedWindow("Frame", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Frame", 2448, 2080)
 
-# cv.imshow('Frame', img)
-# cv.imshow('FG MASK Frame', fgmask)
+cv2.namedWindow("FG MASK Frame", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("FG MASK Frame", 2448, 2080)
+for img in All_images:
 
-plt.subplot(2,1,1)
-plt.imshow(img)
-plt.subplot(2,1,2)
-plt.imshow(fgmask)
+    if img is None:
+        break
+    fgmask = fgbg.apply(img)
+    #fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
-plt.show()
+    cv2.imshow('Frame', img)
+    cv2.imshow('FG MASK Frame', fgmask)
 
-keyboard = cv.waitKey(0)
-if keyboard == 'q' or keyboard == 27:
-    cv.destroyAllWindows()
+    cv2.waitKey(0)
+    
+
+cv2.destroyAllWindows()
